2bcfb050756c0a65719cf1de61bc6f0b314b6747
Austin Song <austinhssong@gmail.com>
2023-08-10 12:23:36 -0400

    express now passing data but console.log undef

--- server/server.js
+++ server/server.js
@@ -3,8 +3,17 @@ const app = express();
 const bodyParser = require('body-parser')
 const players = require('./routes/players')
 
+// const circularJSON = require('circular-json');
+
+
 
 const PORT = process.env.PORT || 8080
+
+//to access info from forms
+app.use(bodyParser.urlencoded({ extended: true }));
+//allows you to use data from react via json objects
+app.use(bodyParser.json());
+app.use(bodyParser.raw());
 
 const cors = require('cors');
 
@@ -13,20 +22,21 @@ const corsOption = {
 };
 app.use(cors(corsOption));
 
-app.use(cors())
 
-//to access info from forms
-app.use(bodyParser.urlencoded({ extended: true }));
-//allows you to use data from react via json objects
-app.use(bodyParser.json());
-app.use(bodyParser.raw());
 
 
 // https://balldontlie.io/api/v1/players?search=lebronjames
 
 app.get(`/api/v1/`, (req, res) => {
-  // console.log(req)
-  res.send(req);
+//   try {
+//     let json = circularJSON.stringify(req);
+//     res.send(json);
+// } catch(e) {
+//     console.log(e);
+//     res.send({ error: e.message });
+// }
+  res.send(req.body);
+  console.log(req.body)
 });
 
 // app.use(`/api/v1/players?search=${lastName}`, players)
